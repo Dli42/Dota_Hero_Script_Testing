1f1a88444e7a8181efef5d2448af1c2eeb0aa138
DavidLi <davidl@gaumard.com>
2014-08-25 16:02:43 -0400

    Added push and blodotest abilities, untested

--- scripts/vscripts/custom_scripted_abilities.lua
+++ scripts/vscripts/custom_scripted_abilities.lua
@@ -466,3 +466,22 @@ function RiflemanRocketJump(keys)
     end)
 end
 
+function BloodTest(keys)
+    local target = keys.target
+    if target:HasModifier("modifier_infected_blood") then
+        ShowMessage("This person is the Thing!")
+    else
+        ShowMessage("No reaction...")
+    end    
+end
+
+function WhisperToTarget(keys)
+    local caster = keys.caster
+    local target = keys.target
+    local myString = keys.String
+    local teamID = target:GetTeamID()
+    local playerID = target:GetPlayerOwnerID()
+    
+    GameRules:SendCustomMessage(myString,teamID,playerID)    
+end
+
